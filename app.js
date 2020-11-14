151b4be0b4ae59bea544906f70f0b956127073d9
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-14 15:30:16 -0600

    added two routes and inserted two seed todos to display when route is hit

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ app.use(bodyParser.json());
 const db = require("./db");
 const collection = "todo";
 
+// ROUTES
+// Route to show index.html file at /
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route to get all todos from collection and display them in an array on the page /getTodos
+app.get('/getTodos',(req,res)=>{
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+            else{
+                console.log(documents);
+                res.json(documents);
+            }
+    });
+})
+
 db.connect((err)=>{
     if(err){
         console.log('unable to connect to database');
